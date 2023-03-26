d03f5fb3242da18289269b0e462ba88af61509ed
PplTanayGondaliya <tanaygondaliya12@gmail.com>
2023-03-26 15:11:58 +0530

    update

--- CV-Mate/app/src/main/java/com/example/cv_mate/Profile.java
+++ CV-Mate/app/src/main/java/com/example/cv_mate/Profile.java
@@ -2,13 +2,31 @@ package com.example.cv_mate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class Profile extends AppCompatActivity {
+
+    TextView personalDetail;
+    Dialog personalDetailDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        personalDetail = findViewById(R.id.profile_personal_detail);
+
+        personalDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                personalDetailDialog.setContentView(R.layout.personal_detail_popup);
+                personalDetailDialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+            }
+        });
+
     }
 }
